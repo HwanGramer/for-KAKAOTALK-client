7721530403a3db83d21f46be49104dd7f28f50fc
mang <mang4371@gmail.com>
2022-11-26 21:28:11 +0900

    1126

--- src/pages/ChatPage/PrivateChatPage.js
+++ src/pages/ChatPage/PrivateChatPage.js
@@ -1,20 +1,30 @@
 import axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import './ChatPage.css';
 
 function PrivateChatPage() {
     const {receiver} = useParams(); //? receiver유저에게 챗을 보내야됨.
 
+    const [chatMsg , setChatMsg] = useState('');
+
 //* ------------------------------------------------------------------------------------------ 
     // useEffect(()=>{ //? 로그인되지 않았거나 채팅할 유저가 일치하지않으면 브라우저는 닫힘
     //     axios.post('/api/user/check' , {userId : receiver}).then((result)=>{
-    //         if(!result.data.suc){ //? 유저가 없으면 강제종료 
-    //             alert(result.data.msg);
-    //             return window.close();
-    //         } 
+    //         if(!result.data.suc){alert(result.data.msg); return window.close();} //? 유저가 없으면 강제종료 
+
+    //         //? 대화 내용 불러와야됨. 
     //     })
     // })
+//* ------------------------------------------------------------------------------------------
+
+
+//* ------------------------------------------------------------------------------------------
+
+    const SendChat = ()=>{
+        console.log(chatMsg);
+    }
+
 //* ------------------------------------------------------------------------------------------
     
   return (
@@ -27,34 +37,22 @@ function PrivateChatPage() {
             <div className='chatRoomName'>사용자이름</div>
         </div>
 
+
         <div className='chatRoomBody'>
 
         </div>
 
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
-        {/* 채팅창 완성하기 클릭했을때 input창좀 바꾸셈 */}
 
         <div className='chatFooter'>
-            <textarea>
+
+            <textarea value={chatMsg} onChange={(e)=>{setChatMsg(e.target.value)}}>
                 
             </textarea>
-            <div className='ChatsendBtn'>
+
+            <div className='ChatsendBtn' onClick={SendChat}>
                 전송
             </div>
+
         </div>
 
     </div>
